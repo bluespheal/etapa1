b7e576e64d9363fd72469718ef44a5e4b7ba1d7a
phacohen <bluespheal@gmail.com>
2017-01-26 18:29:59 -0600

    Avance en prueba de sopa de letras

--- jueves/prueba.rb
+++ jueves/prueba.rb
@@ -1,35 +1,74 @@
-def hola
-  array = ["hola", "hoxxxhola"]
-  letter = 0
-  position = 0
-  hola = true
-  while letter < array[1].length
-    if array[0][position] == array[1][letter]  
-      while hola == true
-        if array[0][position + 1] == array[1][letter+1]
-          p position += 1
-          p letter += 1
-          p array[0][position]
-        else
-          p position += 1
-          p "falso"
-          hola = false
-        end
-      end
+def finder
+  array= ["hola", "xxxholaxxxholxxxhxalohhohh"]
+  horizontal(array)
+  horizontal_rev(array)
+end
 
-      letter += 1
-    # else
-    #   letter += 1
+def horizontal(array)
+let = 0
+  base = 0
+  while let < array[1].length
+    if array[0][base] == array[1][let]
+      p  let , array[1][let]
+      let += 1 
+      base += 1
+    else
+      let += 1
     end
-    letter += 1
   end
 end
 
-#if array[0][0 + 1] == array[1][letter+1] 
-      #   p "si"
-      #   if array[0][1 + 1] == array[1][5+1] 
-      #     p "van bien"
-      #   end
-      #end
+def horizontal_rev(array)
+  let = array[1].length
+    base = 0
+    while let > 0
+      if array[0][base] == array[1][let]
+        p  let , array[1][let]
+        let -= 1 
+        base += 1
+      else
+        let -= 1
+      end
+    end
+end
 
-p hola
+
+finder
+
+
+
+
+
+# def hola
+#   array = ["hola", "hoxxxxxxholaxxxxxxxholaxxxx"]
+#   letter = 0
+#   while letter < array[1].length
+#     position = 0
+#     if array[0][position] == array[1][letter]
+#       while position < array[0].length
+#         if array[0][position] == array[1][letter+1]
+#           p array[0][position]
+#           p letter += 1 
+#           #position += 1
+
+#         else
+#           position += 1
+#         end
+#       end
+
+#       letter += 1
+#     # else
+#     #   letter += 1
+#     end
+#     letter += 1
+#   end
+# end
+
+# #if array[0][0 + 1] == array[1][letter+1] 
+#       #   p "si"
+#       #   if array[0][1 + 1] == array[1][5+1] 
+#       #     p "van bien"
+#       #   end
+#       #end
+
+# p hola
