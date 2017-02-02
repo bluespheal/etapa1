7c76dce80c29a9ff4a46c432a09ca9c281aa5d93
phacohen <bluespheal@gmail.com>
2017-02-02 17:36:12 -0600

    Ejercicios del jueves 2 de Febrero

---
+++ semana2/jueves/ruby_todo_mvc.rb
@@ -0,0 +1,81 @@
+require 'csv'
+
+class ListParser
+  def self.list_view
+    CSV.foreach("list.csv").with_index(1) do |row, ln|
+      puts ln.to_s + ". " + row.join
+    end
+  end
+end
+
+class ListIndex
+  def self.show_list
+    if ARGV[0] == "index"
+      ListParser.list_view
+    end
+  end
+end
+
+class ListAdder
+  def self.add_item
+    if ARGV[0] == "add"
+      string = ARGV[1..-1].map { |x| " #{x}" + " " }.unshift("[ ]")
+      CSV.open("list.csv", "ab") do |csv|
+        csv << string
+      end
+      p "Agregaste la tarea: '#{string.join}' a tu lista."
+    end
+  end
+end
+
+class ListDeleter 
+  def self.delete_item
+    if ARGV[0] == "delete" # "=" NO!!!! "=="!!!!
+      array = CSV.read("list.csv")
+      array.each_with_index do |x, i|
+        if ARGV[1].to_i - 1 == i
+          array.delete(x)
+        end
+      end
+      CSV.open("list.csv", 'wb') do |csv|
+        array.map do |i|
+          csv << i
+        end
+      end 
+      ListParser.list_view
+    end
+  end
+end
+
+class ListCompleter 
+  def self.complete_item
+    if ARGV[0] == "complete"
+      array = CSV.read("list.csv")
+      array.each_with_index do |x, i|
+        if ARGV[1].to_i - 1 == i
+          x[0].gsub!(" ", "X")
+          x[0][1]
+        end
+      end
+      CSV.open("list.csv", 'wb') do |csv|
+        array.map do |i|
+          csv << i
+        end
+      end 
+      ListParser.list_view
+    end
+  end
+end
+
+class List
+  ListIndex.show_list
+  ListAdder.add_item
+  ListDeleter.delete_item
+  ListCompleter.complete_item
+  
+end
+
+list = List.new
+list
+
+
