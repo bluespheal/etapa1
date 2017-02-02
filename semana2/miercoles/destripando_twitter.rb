0d8d077180edf490670fed219f5dee8f77e8138c
phacohen <bluespheal@gmail.com>
2017-02-01 18:17:51 -0600

    Ejercicios del Miercoles 1 de Febrero

--- semana2/miercoles/destripando_twitter.rb
+++ semana2/miercoles/destripando_twitter.rb
@@ -1,35 +1,47 @@
 require 'nokogiri'
+require 'open-uri'
 
-def username
-  doc = Nokogiri::HTML(File.open('twitter_account.html'))
-  username = doc.search(".ProfileHeaderCard-name > a")
-  puts "Username: #{username.first.inner_text}"
+class TwitterScrapper
+  def initialize
+    @url = ARGV[0]
+    @doc = Nokogiri::HTML(open(@url))
+  end
+
+  def extract_username
+    @username = @doc.search(".ProfileHeaderCard-name > a").first.inner_text
+  end
+
+  def extract_tweets
+    @tweet_text = @doc.search('.tweet-text')
+    @tweet_date = @doc.search('._timestamp')
+    @retweets = @doc.search('[data-modal="ProfileTweet-retweet"] .ProfileTweet-actionCountForPresentation')
+    @favorited = @doc.search('.js-actionFavorite .ProfileTweet-actionCountForPresentation')
+  end
+
+  def extract_stats
+    @tweets = @doc.search('[data-nav="tweets"] > .ProfileNav-value').first.inner_text
+    @siguiendo = @doc.search('[data-nav="following"] > .ProfileNav-value').first.inner_text
+    @seguidores = @doc.search('[data-nav="followers"] > .ProfileNav-value').first.inner_text
+    @favoritos = @doc.search('[data-nav="favorites"] > .ProfileNav-value').first.inner_text
+  end
+
+  def printer
+    puts "\e[#{1}m#{"\e[#{5}m#{"\e[#{7}m#{"\e[#{36}m#{"Username: #{@username}"}"}"}"}\e[0m"
+    status_bar = "Stats: Tweets: #{@tweets}, Siguiendo: #{@siguiendo}, Seguidores: #{@seguidores}, Favoritos: #{@favoritos}"
+    puts "\u2554" + "\u2550" * status_bar.length + "\u2557" + "\n" + "\u2551" + status_bar + "\u2551" + "\n" + "\u255A" + "\u2550" * status_bar.length + "\u255D"
+    puts "\e[#{4}m#{"Tweets:"}\e[0m\n\n"
+    for i in 0..2
+      puts "\e[#{47}m#{"#{@tweet_date[i].inner_text}.: #{@tweet_text[i].inner_text}"}\e[0m\n" + "\e[#{47}m#{"\e[#{36}m#{"\u267B"} #{@retweets[i * 2].inner_text}, \e[#{31}m#{"\u2665"} #{@favorited[i * 2].inner_text}"}\e[0m \n\n"
+    end
+  end
 end
 
-def stats
-  doc = Nokogiri::HTML(File.open('twitter_account.html'))
-  tweets = doc.search('[data-nav="tweets"] > .ProfileNav-value')
-  tweets = tweets.first.inner_text
-  siguiendo = doc.search('[data-nav="following"] > .ProfileNav-value')
-  siguiendo = siguiendo.first.inner_text
-  seguidores = doc.search('[data-nav="followers"] > .ProfileNav-value')
-  seguidores = seguidores.first.inner_text
-  favoritos = doc.search('[data-nav="favorites"] > .ProfileNav-value')
-  favoritos = favoritos.first.inner_text
-  puts "Stats: Tweets: #{tweets}, Siguiendo: #{siguiendo}, Seguidores: #{seguidores}, Favoritos: #{favoritos}"
-end
+prueba = TwitterScrapper.new
 
-def tweets
-  doc = Nokogiri::HTML(File.open('twitter_account.html'))
-  texto = doc.search('.tweet-text')
-  fecha = doc.search('._timestamp')
-  retweet = doc.search('[data-modal="ProfileTweet-retweet"] .ProfileTweet-actionCountForPresentation')
-  favoritos = doc.search('.js-actionFavorite .ProfileTweet-actionCountForPresentation')
-  puts "Tweets:\n#{fecha[0].inner_text}.: #{texto[0].inner_text}\n\u267B #{retweet[0].inner_text}, \u2665 #{favoritos[0].inner_text}\n\n"
-  puts "#{fecha[1].inner_text}.: #{texto[1].inner_text}\n\u267B #{retweet[2].inner_text}, \u2665 #{favoritos[2].inner_text}\n\n"
-  puts "#{fecha[2].inner_text}.: #{texto[2].inner_text}\n\u267B #{retweet[4].inner_text}, \u2665 #{favoritos[4].inner_text}"
-end
+prueba.extract_username
+prueba.extract_stats
+prueba.extract_tweets
 
-username
-stats
-tweets
+prueba.printer
+
+
